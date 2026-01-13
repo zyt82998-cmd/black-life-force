26eb00a0ff72ba2e7e0bb698a997f11f1500aa76
zyt82998-cmd <zyt82998@gmail.com>
2026-01-13 20:00:14 +0800

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "blf-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
